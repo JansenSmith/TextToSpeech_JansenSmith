860fdaad8c958754e589ba492eaa9ea0c7adab84
madhephaestus <mad.hephaestus@gmail.com>
2016-05-08 14:45:25 -0400

    text to speech example

--- TextToSpeech.groovy
+++ TextToSpeech.groovy
@@ -1,1 +1,52 @@
 //Your code here
+BowlerKernel.speak(
+	"This is the default voice",
+	175, //rate 
+	120, //pitch
+	41,  //range
+	1,   //shift
+	1 	//volume
+)
+BowlerKernel.speak(
+	"but bowler studio",
+	275, //rate 
+	120, //pitch
+	41,  //range
+	2,   //shift
+	0.75 	//volume
+)
+BowlerKernel.speak(
+	"can make",
+	75, //rate 
+	220, //pitch
+	81,  //range
+	0.5,   //shift
+	1 	//volume
+)
+
+BowlerKernel.speak(
+	"Lots of",
+	125, //rate 
+	120, //pitch
+	41,  //range
+	1,   //shift
+	1 	//volume
+)
+
+BowlerKernel.speak(
+	"differents kinds",
+	175, //rate 
+	220, //pitch
+	41,  //range
+	2,   //shift
+	1 	//volume
+)
+
+BowlerKernel.speak(
+	"of voices",
+	175, //rate 
+	120, //pitch
+	41,  //range
+	0.05,   //shift
+	1 	//volume
+)
