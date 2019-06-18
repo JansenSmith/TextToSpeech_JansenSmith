43521d806b6c5a44c4c99975efb26ec9cc8c1800
Kevin Harrington <mad.hephaestus@gmail.com>
2019-06-18 10:44:25 -0400

    updated demo

--- TextToSpeech.groovy
+++ TextToSpeech.groovy
@@ -1,52 +1,31 @@
-//Your code here
 BowlerKernel.speak(
-	"This is the default voice",
-	175, //rate 
-	120, //pitch
-	41,  //range
-	1,   //shift
+		"This is the default voice");
+BowlerKernel.speak("Voice one", 100, 0, 300, 1.0, 1.0);
+BowlerKernel.speak("Voice two", 100, 0, 200, 1.0, 1.0);
+BowlerKernel.speak("Voice three", 100, 0, 100, 1.0, 1.0);
+BowlerKernel.speak("Voice four", 100, 0, 50, 1.0, 1.0);
+BowlerKernel.speak(
+	"we are the borg; resistance is futile",
+	100, //rate 
+	0, //slur
+	300,  //voice
+	1,   //echo
+	0.4 	//volume
+);
+BowlerKernel.speak(
+	"An echoing voice in a stadium",
+	300, //rate 
+	0, //slur
+	300,  //voice
+	0.5,   //echo
 	1 	//volume
-)
+);
+////
 BowlerKernel.speak(
-	"but bowler studio",
-	275, //rate 
-	120, //pitch
-	41,  //range
-	2,   //shift
-	0.75 	//volume
-)
-BowlerKernel.speak(
-	"can make",
-	75, //rate 
-	220, //pitch
-	81,  //range
-	0.5,   //shift
-	1 	//volume
-)
-
-BowlerKernel.speak(
-	"Lots of",
-	125, //rate 
-	120, //pitch
-	41,  //range
-	1,   //shift
-	1 	//volume
-)
-
-BowlerKernel.speak(
-	"differents kinds",
-	175, //rate 
-	220, //pitch
-	41,  //range
-	2,   //shift
-	1 	//volume
-)
-
-BowlerKernel.speak(
-	"of voices",
-	175, //rate 
-	120, //pitch
-	41,  //range
-	0.05,   //shift
-	1 	//volume
-)
+		"The robot voice of starscream",
+		100, //rate 
+		90, //slur
+		200,  //voice
+		1,   //echo
+		1 	//volume
+);
